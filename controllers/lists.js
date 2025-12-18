08fe2bed0253148fe246d9cbe16f45eb5ebce8d9
Nick Choate <nickchoate@berkeley.edu>
2025-12-17 23:44:27 -0800

    edit function is bug free

--- controllers/lists.js
+++ controllers/lists.js
@@ -7,7 +7,7 @@ const List = require('../models/list.js');
 const isSignedIn = require('../middleware/is-signed-in.js');
 
 // transforms list of zips from string to list of objects
-function parser(locationsString) {
+function parseList(locationsString) {
   if (locationsString && locationsString.includes('#')) {
     let locationsList = []
     let split = locationsString.split('#')
@@ -60,6 +60,41 @@ async function validateZip(req) {
   return [draft, message]
 }
 
+// parse req object and store in session object
+function parseSession(reqBody) {
+  draft = {
+    name: reqBody.name.trim(),
+    description: reqBody.description,
+    locations: parseList(reqBody.locations),
+    zip: reqBody.zip,
+    source: reqBody.source,
+  }
+  return draft
+}
+
+async function validateListName(reqBody, reqSession) {
+  // reject nameless list while retaining draft data, GET /new
+  if (!reqBody.name || !reqBody.name.trim()) {
+    return [false, `${reqBody.source}?draft=1&error=name`]
+  }
+
+  console.log(reqBody)
+  console.log(reqSession)
+
+  // reject if user already has a list with this name, GET /new
+  const selectedUser = await User.findOne({ _id: reqSession.user._id });
+  const populatedLists = await List.find({
+    owner: selectedUser })
+    .populate('owner');
+  for (let list of populatedLists) {
+    if (list.name.toLowerCase() == reqSession.draft.name.toLowerCase()) {
+      if (list._id == reqBody.source.split('/')[2]) break
+      return [false, `${reqBody.source}?draft=1&error=duplicate`]
+    }
+  }
+  return [true]
+}
+
 
 
 router.get('/new', isSignedIn, async (req, res) => {
@@ -68,10 +103,8 @@ router.get('/new', isSignedIn, async (req, res) => {
     let draft = output[0]
     let message = output[1]
 
-    // if (draft && (draft.source !== '/lists/new')) {
-    //   return res.render('lists/edit.ejs', { list: draft, message })
-    // }
     res.render('lists/new.ejs', { draft, message })
+    
   } catch (error) {
     console.log(error);
     res.redirect('/lists/new?draft=1');
@@ -80,43 +113,18 @@ router.get('/new', isSignedIn, async (req, res) => {
 
 router.post('/', isSignedIn, async (req, res) => {
   try {
-    // store draft data in sessions object
-    req.session.draft = {
-      name: req.body.name.trim(),
-      description: req.body.description,
-      locations: parser(req.body.locations),
-      zip: req.body.zip,
-      source: req.body.source,
-    }
+    console.log(req.body)
 
-    // potentially remove location from draft, GET /new
+    req.session.draft = await parseSession(req.body)
+
     if (req.body.add || req.body.remove) {
-      if (req.body.remove) {
-        req.session.draft.locations.splice(Number(req.body.remove), 1)
-      }
-      if (req.session.draft.source !== '/lists/new') {
-        return res.redirect(`${req.body.source}?draft=1`)
-      }
+      if (req.body.remove) req.session.draft.locations.splice(Number(req.body.remove), 1)
       return res.redirect(`/lists/new?draft=1`)
     }
 
-    // reject nameless list while retaining draft data, GET /new
-    if (!req.body.name || !req.body.name.trim()) {
-      return res.redirect('/lists/new?draft=1&error=name');
-    }
-
-    // reject if user already has a list with this name, GET /new
-    const selectedUser = await User.findOne({ _id: req.session.user._id });
-    const populatedLists = await List.find({
-      owner: selectedUser })
-      .populate('owner');
-    for (let list of populatedLists) {
-      if (list.name.toLowerCase() == req.session.draft.name.toLowerCase()) {
-        return res.redirect('/lists/new?draft=1&error=duplicate')
-      }
-    }
+    let nameValid = await validateListName(req.body, req.session)
+    if (!nameValid[0]) return res.redirect(nameValid[1])
     
-    // create and save list to database
     const newList = new List(req.session.draft);
     newList.owner = req.session.user._id;
     await newList.save();
@@ -189,15 +197,19 @@ router.get('/:listId/edit', isSignedIn, async (req, res) => {
 
 router.put('/:listId', isSignedIn, async (req, res) => {
   try {
-    console.log('req: ')
-    console.log(req.body)
-    console.log('listID: ')
-    console.log(req.params.listId)
+    req.session.draft = await parseSession(req.body)
+
+    if (req.body.add || req.body.remove) {
+      if (req.body.remove) req.session.draft.locations.splice(Number(req.body.remove), 1)
+      return res.redirect(`${req.body.source}?draft=1`)
+    }
+
+    let nameValid = await validateListName(req.body, req.session)
+    if (!nameValid[0]) return res.redirect(nameValid[1])
 
     const currentList = await List.findById(req.params.listId);
-    
     if (currentList.owner.equals(req.session.user._id)) {
-      await currentList.updateOne(req.body);
+      await currentList.updateOne(req.session.draft);
       res.redirect(`/lists/${req.params.listId}`);
     } else {
       res.send("You don't have permission to do that.");
